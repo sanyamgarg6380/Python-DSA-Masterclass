34dcb99769e061f6e1637784b6278dc2a49e036e
sanyamgarg6380 <sanyamgarg7011@gmail.com>
2024-10-22 23:54:45 +0530

    Code for reversing an array

---
+++ ReverseAnArray.py
@@ -0,0 +1,13 @@
+def reverseArray(arr):
+    i=0
+    j=len(arr)-1
+    while i<j:
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        i+=1
+        j-=1
+    return arr
+
+arr={1,2,3,4}
+print(reverseArray(arr))
